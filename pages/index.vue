bf794f75fc1c8d8db20941cd260b965282710f86
Tom <tmaltby19@gmail.com>
2023-12-17 10:54:46 +0700

    init

--- pages/index.vue
+++ pages/index.vue
@@ -5,3 +5,15 @@
         <h1>Welcome to the homepage</h1>
     </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+    title: 'The Elephant Queen',
+    meta: [
+        {
+            name: 'description',
+            content: 'Watch The Elephant Queen in English, Kiswahili or Maa.',
+        },
+    ],
+})
+</script>
---
+++ pages/mobile-cinema.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome to the mobile cinema page</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'The Elephant Queen: Mobile Cinema',
+    meta: [
+        {
+            name: 'description',
+            content:
+                "Taking 'The Elephant Queen' to the children & people of Kenya",
+        },
+    ],
+})
+</script>
